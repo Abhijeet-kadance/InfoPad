ebbf7704c9c57c0bdca092436f2afeccaab49d72
Abhijeet Thorat <abhijeett@gist.cdacitpark.in>
2022-09-23 13:18:42 +0530

    (IDEAPAD) => Created Basic Template Setup and views

--- base/views.py
+++ base/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+rooms = [
+    {'id':1, 'name':'Lets Learn Python'},
+    {'id':2, 'name':'Lets Learn JavaScript'},
+    {'id':3, 'name':'Lets Learn Go Language'},
+]
+def home(request):
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html',context)
+
+
+def room(request):
+    return render(request,'base/room.html',{'rooms':rooms})
--- ideapad/settings.py
+++ ideapad/settings.py
@@ -48,7 +48,9 @@ ROOT_URLCONF = 'ideapad.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'    
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
