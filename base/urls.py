83c0473a3c0dd6de180ae7b29e4d3fd597db2b70
Abhijeet-kadance <abhijeet.kadance.96@gmail.com>
2022-09-24 21:23:46 +0530

    (IDEAPAD) => Added Linting

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/',views.loginPage,name="login"), 
+    
     path('logout/',views.logoutUser,name="logout"), 
     path('register/',views.registerUser,name="register"), 
     path('', views.home , name="home"), 
