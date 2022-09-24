c4bcc0141d8fcf50973829f04721e74aa8df6b4e
Abhijeet-kadance <abhijeet.kadance.96@gmail.com>
2022-09-24 23:00:47 +0530

    (IDEAPAD) => linting is set to off" "

--- base/urls.py
+++ base/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/' , views.loginPage , name="login") , 
-
-    path('logout/' , views.logoutUser , name="logout") , 
+    path('login/', views.loginPage, name="login"), 
+    path('logout/' , views.logoutUser , name="logout"), 
    
     path('register/' , views.registerUser , name="register") , 
    
