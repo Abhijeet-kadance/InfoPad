60606cf3cee4997f2fd76860b32776aa7768ec5c
Abhijeet-kadance <abhijeet.kadance.96@gmail.com>
2022-09-26 11:49:55 +0530

    (IDEAPAD) => Created basic chatroom functionality

--- base/views.py
+++ base/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import authenticate , login , logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.db.models import Q
-from .models import Room , Topic
+from .models import Room , Topic, Message
 from .forms import RoomForm
 
 
@@ -77,8 +77,33 @@ def home(request):
 
 def room(request , pk):
     room = Room.objects.get(id=pk)
-    context = {'room' : room}
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+
+    context = {'room' : room , 'room_messages':room_messages , 'participants':participants}
     return render(request , 'base/room.html' , context)
+
+@login_required(login_url="/login")
+def deleteMessage(request,pk):
+    message = Message.objects.get(id=pk)
+
+    
+    if request.user != message.user :
+        return HttpResponse('Your are not Allowed to Delete This Message...')
+    
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request,'base/delete.html', {'obj':message})
+
 
 @login_required(login_url="/login")
 def createRoom(request):
