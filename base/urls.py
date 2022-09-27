ae7314ebb6ecdd27dabd6e87ed4b0bcaf7dbe9c5
Abhijeet-kadance <abhijeet.kadance.96@gmail.com>
2022-09-27 15:46:32 +0530

    (IDEAPAD) => Register/Login/Chat Room UI Updated

--- base/urls.py
+++ base/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('update-room/<str:pk>/' , views.updateRoom , name="update-room") , 
     path('delete-room/<str:pk>/' , views.deleteRoom , name="delete-room") , 
     path('delete-message/<str:pk>/' , views.deleteMessage , name="delete-message") , 
+    path('update-user' , views.updateUser , name="update-user") , 
     path('profile/<str:pk>',views.userProfile,name="user-profile")
     # path('/<str:pk>/',views.deleteRoom , name="delete-room"),
 ]
--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.db.models import Q
 from .models import Room , Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm ,UserForm
 
 
 # Create your views here.
@@ -171,3 +171,17 @@ def deleteRoom(request,pk):
         return redirect('home')
     return render(request,'base/delete.html', {'obj':room})
 
+@login_required(login_url="/login")
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+    
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile',pk=user.id)
+    context = {
+        "form":form,
+    }
+    return render(request,'base/update-user.html',context)
