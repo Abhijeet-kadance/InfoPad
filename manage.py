abe59c3caf1d3ebee0baabfcd006f1ce7688e910
Abhijeet-kadance <abhijeet.kadance.96@gmail.com>
2023-03-29 11:04:44 +0530

    Updated manage.py file

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import dotenv
+# from dotenv import load_dotenv
 
 def main():
     """Run administrative tasks."""
@@ -18,6 +18,6 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
-    dotenv.load_dotenv()
-    main()
+# if __name__ == '__main__':
+#     dotenv.load_dotenv()
+#     main()
