aaff558839a701e0f83360f1542839cd5b70935f
Abhijeet-kadance <abhijeet.kadance.96@gmail.com>
2023-03-29 12:42:48 +0530

    Updated nginx setup

--- nginx.sh
+++ nginx.sh
@@ -2,11 +2,12 @@
  
 echo "NGINX SETUP: INITIALIZED...."
 
-sudo cp -rf  app.conf /etc/nginx/conf.d
-
+sudo cp -rf  app.conf /etc/nginx/sites-available/app
 chmod 710 /var/lib/jenkins/workspace/django-cicd
 
-sudo nginx -t 
+sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
+sudo nginx -t
+
 
 sudo systemctl start nginx
 sudo systemctl enable nginx
