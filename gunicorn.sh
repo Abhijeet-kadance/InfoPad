4a2394445a9fdf0686df35a4e872b5edeb749cc3
Abhijeet-kadance <abhijeet.kadance.96@gmail.com>
2023-03-29 11:40:38 +0530

    Created gunicorn socket and service

--- gunicorn.sh
+++ gunicorn.sh
@@ -8,3 +8,20 @@ python3 manage.py makemigrations
 python3 manage.py migrate
 
 echo "Migration done"
+
+cd /var/lib/jenkins/workspace/django-cicd/
+
+sudo cp -rf gunicorn.socket /etc/systemd/system/
+sudo cp -rf gunicorn.service /etc/systemd/system/
+
+echo "$USER"
+echo "$PWD"
+
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
+
+echo "Gunicorn has been started"
+
+sudo systemctl status gunicorn
+sudo systemctl restart gunicorn
